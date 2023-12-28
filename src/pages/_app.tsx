36e98d5a1ab3b00e8a68a99e779b6678ff4ab83f
Hodong <fire13764@gmail.com>
2023-12-28 22:32:58 +0900

    Comment: layout 관련 TODO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import DefaultLayout from '@/components/layout/DefaultLayout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
+// TODO: 레이아웃 Comp 증가 or 페이지 마다 다른 레이아웃이 필요한 경우 getLayout 함수를 통해 레이아웃을 가져오는 방식으로 변경
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <DefaultLayout>
